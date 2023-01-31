245ed48ce9497a9c1df26dc774d82afc989c23c7
Chinmay Bansal <chinmaybansalsc@outlook.com>
2023-01-31 15:52:48 -0500

    Commit

---
+++ flixster/MovieCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieCell.swift
+//  flixster
+//
+//  Created by Chinmay Bansal on 1/31/23.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
